d00e32758d21433e2ccd3dccf3e50a3455c04bab
dave <titusdavid39@gmail.com>
2023-04-29 01:28:24 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,8 @@
+let hamburger = document.querySelector(".ham");
+let navList = document.querySelector(".nav-list");
+let links = document.querySelector(".nav-list li");
+
+hamburger.addEventListener("click", function () {
+    this.classList.toggle("click");
+    navList.classList.toggle("open");
+    });
